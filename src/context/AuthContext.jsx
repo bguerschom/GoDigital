eeb2ec75817ede9540ad7dfd01c62132ca417a73
bguerschom <148221717+bguerschom@users.noreply.github.com>
2024-12-13 18:00:55 +0200

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -57,5 +57,9 @@ export const AuthProvider = ({ children }) => {
 }
 
 export const useAuth = () => {
-  return useContext(AuthContext)
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider')
+  }
+  return context
 }

e3806f7312f24d5be66b09682b756d3e3b31cd3d
bguerschom <148221717+bguerschom@users.noreply.github.com>
2024-12-13 17:53:12 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -6,17 +7,27 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-build: {
-  rollupOptions: {
-    output: {
-      globals: {
-        recharts: 'Recharts',
-        xlsx: 'XLSX'
+  server: {
+    port: 3000,
+    open: true,
+    host: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'terser',
+    chunkSizeWarningLimit: 1500,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
       }
     }
   }
-}
 })
